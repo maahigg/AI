6b89fa8b25380a976446173a09419ac4cee47ad9
maahigg <gera.maahi@gmail.com>
2025-07-31 13:47:44 +0400

    rotatrion

---
+++ rotateandcrop.py
@@ -0,0 +1,44 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+image = cv2.imread('download.jpg')
+
+image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+plt.imshow(image_rgb)
+plt.title('original img')
+plt.show()
+
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GAY)
+plt.imshow(gray_image, cmap = 'gray')
+plt.title('grayscale image')
+plt.show()
+
+cropped_image = image[100:300, 200:400]
+
+cropped_rgb = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2RGB)
+plt.imshow(cropped_rgb)
+plt.title('cropped region')
+plt.show()
+
+(h, w) = image.shape[:2]
+center = (w//2, h//2)
+M = cv2.getRotationMatrix2D(center, 45, 1.0)
+rotated = cv2.warpAffine(image, M, (w, h))
+rotated_rgb = cv2.cvtColor(rotated, cv2.COLOR_BGR2RGB)
+plt.imshow(rotated_rgb)
+plt.title('rotated img')
+plt.show()
+
+brightness_matrix = np.ones(image.shape, dtype="uint8" )*50
+brighter = cv2.add(image, brightness_matrix)
+brighter_rgb = cv2.cvtColor(brighter, cv2.COLOR_BGR2RGB)
+plt.imshow(brighter_rgb)
+plt.title('brighter img')
+plt.show()
+
+cv2.imwrite('grayimg.jpg',gray_image)
+cv2.imwrite('croppedimg.jpg',cropped_image)
+cv2.imwrite('rotatedimg.jpg',rotated)
+cv2.imwrite('brighterimg.jpg',brighter)
