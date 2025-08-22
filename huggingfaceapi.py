d7620f88cda39cfb652bc94d7f3d4ac2b1a363c2
Maahi Gera <gera.maahi@gmail.com>
2025-08-22 10:44:51 +0400

    Add files via upload

---
+++ huggingfaceapi.py
@@ -0,0 +1,16 @@
+import requests
+
+api_url = "https://api-inference.huggingface.co/models/distilbert-base-uncased-finetuned-sst-2-english"
+
+headers = {
+    "Authorization": "Bearer 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"
+}
+
+text = "i love learning about ai it is so fascinating"
+
+response = requests.post(api_url, headers=headers, json={"inputs": text})
+if response.status_code == 200:
+    classification = response.json()
+    print("predicted label: ", classification[0]['label'])
+else:
+    print(f"error: {response.status_code}")
